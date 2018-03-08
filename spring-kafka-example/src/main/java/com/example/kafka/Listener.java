e17645c041a0ad1c9c253084bbb0652f4658e18c
David Bingham <david.r.bingham@gmail.com>
2018-03-08 18:18:41 +0000

    Adding examples to experiment with

---
+++ spring-kafka-example/src/main/java/com/example/kafka/Listener.java
@@ -0,0 +1,18 @@
+package com.example.kafka;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class Listener {
+
+	public final CountDownLatch countDownLatch1 = new CountDownLatch(1);
+
+	@KafkaListener(id = "foo", topics = "topic1", groupId = "group1")
+	public void listen(ConsumerRecord<?, ?> record) {
+		System.out.println(record);
+		countDownLatch1.countDown();
+	}
+
+}
